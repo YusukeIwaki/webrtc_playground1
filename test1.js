e7f6d71cf1de97c9ec2332ce3b1ba2a97d37b6e7
Yusuke Iwaki <yusuke.iwaki@crowdworks.co.jp>
2017-03-25 14:07:26 +0000

    新しいAPIでかめら
    https://html5experts.jp/mganeko/19728/
    https://developer.mozilla.org/ja/docs/Web/API/MediaDevices/getUserMedia

--- test1.js
+++ test1.js
@@ -1,11 +1,13 @@
 window.onload = function() {
     var video = document.getElementById("my_video");
     video.style.transform = "rotateY(180deg)"
-    navigator.getUserMedia({video: true, audio: false}, function(stream) {
-        console.log(stream);
-        video.autoplay = true;
-        video.src = window.URL.createObjectURL(stream);
-    }, function(err) {
-        console.log(err);
-    });
+    navigator.mediaDevices.getUserMedia({video: true, audio: false})
+        .then(function(stream) {
+            console.log(stream);
+            video.autoplay = true;
+            video.src = window.URL.createObjectURL(stream);
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
 }
